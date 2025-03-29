bd8657063ad607505958f3f1362be9bd2ec709fc
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-29 16:18:17 +0200

    Adjust styling DRY

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import LogoBig from '../../components/logo-big/LogoBig';
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import LogoBig from "../../components/logo-big/LogoBig";
+import { pageState } from "../../state/pageState";
 import "./Home.scss";
 
 interface Props {}
 
 const Home: React.FC<Props> = ({}) => {
+  const setPage = useSetRecoilState(pageState);
+  const go = (to: string) => setPage(to);
+  const goLogin = () => go("login");
+  const goRegister = () => go("register");
+
   return (
-    <div className='page page--home'>
+    <div className="page page--home">
       <LogoBig />
+      <section>
+        <button onClick={goLogin}>login</button> |{" "}
+        <button onClick={goRegister}>register</button>
+      </section>
     </div>
   );
 };
