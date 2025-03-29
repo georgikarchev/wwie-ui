8a4a867a3960404478cfe77dc8fa29b59239b992
Georgi Karchev <georgi.karchev@gmail.com>
2025-03-29 17:54:13 +0200

    Add Login

--- src/components/secondary-menu/SecondaryMenu.tsx
+++ src/components/secondary-menu/SecondaryMenu.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
+import { useIsAuthenticated } from "../../hooks/useIsAuthenticated";
 import { pageState } from "../../state/pageState";
 import "./SecondaryMenu.scss";
 
 const SecondaryMenu: React.FC = () => {
+  const isAuthenticated: boolean = useIsAuthenticated();
   const setPage = useSetRecoilState(pageState);
   const go = (to: string) => setPage(to);
 
@@ -11,9 +13,11 @@ const SecondaryMenu: React.FC = () => {
 
   return (
     <div className="secondary-menu">
-      <button className="button" onClick={goProfile}>
-        <img className="icon" src="src/assets/icons/outlined/person.png" />
-      </button>
+      {isAuthenticated && (
+        <button className="button" onClick={goProfile}>
+          <img className="icon" src="src/assets/icons/outlined/person.png" />
+        </button>
+      )}
     </div>
   );
 };
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useAuth } from "../../services/authService";
+import "./Login.scss";
 
-interface Props {}
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
 
-const Login: React.FC<Props> = ({}) => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log({ email, password });
+    // Call API here
+    login(email, password);
+  };
+
   return (
-    <div>
-      Login
+    <div className="page page--login">
+      <section className="login-form">
+        <form onSubmit={handleSubmit} className="login-form__form">
+          <div className="form-row">
+            <label>Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-row">
+            <label>Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </section>
     </div>
   );
 };
