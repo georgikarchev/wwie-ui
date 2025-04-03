cc1160a9f45e25d6e70bc35911f1d41e9f7b21c1
Georgi Karchev <georgi.karchev@gmail.com>
2025-04-03 14:32:06 +0300

    Adjust menu by user role

--- src/components/main-menu/MainMenu.tsx
+++ src/components/main-menu/MainMenu.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSetRecoilState } from "recoil";
+import { useIsUserAdmin } from "../../hooks/useIsUserAdmin";
 import { pageState } from "../../state/pageState";
 import "./MainMenu.scss";
 
 const MainMenu: React.FC = ({}) => {
+  const isAdmin = useIsUserAdmin();
   const setPage = useSetRecoilState(pageState);
 
   const go = (to: string) => setPage({ name: to });
@@ -21,30 +23,42 @@ const MainMenu: React.FC = ({}) => {
       <button className="button" onClick={goDashboard}>
         <img className="icon" src="src/assets/icons/outlined/home.png" />
       </button>
-      <button className="button" onClick={goAddMeal}>
-        <img className="icon" src="src/assets/icons/outlined/lightbulb.png" />
-      </button>
-      <button className="button" onClick={goHistory}>
-        <img className="icon" src="src/assets/icons/outlined/history.png" />
-      </button>
+      {!isAdmin && (
+        <button className="button" onClick={goAddMeal}>
+          <img className="icon" src="src/assets/icons/outlined/lightbulb.png" />
+        </button>
+      )}
+      {!isAdmin && (
+        <button className="button" onClick={goHistory}>
+          <img className="icon" src="src/assets/icons/outlined/history.png" />
+        </button>
+      )}
+
       <button className="button" onClick={goMeals}>
         <img className="icon" src="src/assets/icons/outlined/restaurant.png" />
       </button>
+
       <button className="button" onClick={goIngredients}>
         <img className="icon" src="src/assets/icons/outlined/extension.png" />
       </button>
-      <button className="button" onClick={goUsers}>
-        <img
-          className="icon"
-          src="src/assets/icons/outlined/people_outline.png"
-        />
-      </button>
-      <button className="button" onClick={goShoppingList}>
-        <img
-          className="icon"
-          src="src/assets/icons/outlined/shopping_cart.png"
-        />
-      </button>
+
+      {isAdmin && (
+        <button className="button" onClick={goUsers}>
+          <img
+            className="icon"
+            src="src/assets/icons/outlined/people_outline.png"
+          />
+        </button>
+      )}
+
+      {!isAdmin && (
+        <button className="button" onClick={goShoppingList}>
+          <img
+            className="icon"
+            src="src/assets/icons/outlined/shopping_cart.png"
+          />
+        </button>
+      )}
     </div>
   );
 };
